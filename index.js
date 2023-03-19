30672cbf8f3d4fa959ae20c0750f2c4261b340f9
Rahul Gandhi <rahulgandhi241172@gmail.com>
2023-03-19 13:08:22 +0530

    project section added

---
+++ index.js
@@ -0,0 +1,22 @@
+let menu=document.getElementById("menu-icon");
+let navbar=document.querySelector(".navbar");
+let m=document.querySelector(".main-body")
+let count=0;
+menu.addEventListener('click',()=>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+    
+    m.style.marginTop="200px";
+    count++;
+    if(count%2==0){
+        m.style.marginTop="0px";
+
+    }
+
+    if(m.classList.contains("m-t")){
+        m.classList.remove("m-t");
+    }
+    else{
+        m.classList.add("m-t");
+    }
+});
